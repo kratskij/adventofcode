3103cda0f35a22f216e3b6d140e203733f0e7ff4
Henrik Svenning Pedersen <henrik.s.pedersen@gmail.com>
2015-12-26 13:33:09 +0000

    day 25, minified

--- 2015/25/nissen.php
+++ 2015/25/nissen.php
@@ -1,23 +1,6 @@
 <?php
-$colz = 3029;
-$rowz = 2947;
-
-#$colz = 3;
-#$rowz = 2;
-
-$next = 20151125;
-$rows = 0;
-while (true) {
-	$rows++;
-	$row = $rows;
-	for ($col = 1; $col < $rows; $col++) {
-		$row = $rows - $col;
-		echo $row." " . $col . " " . $next . "\n";
-		if ($row == $rowz && $col == $colz) {
-			echo $next . "\n";
-			die();
-		}
-		$next = ($next * 252533) % 33554393;
-		
-	}
-}
+$t = 20151125; $r = 2947; $c = 3029; $mul = 252533; $mod = 33554393;
+$x = $r + $c;
+$iter = ($x ** 2 - $x * 3) / 2 + $c;
+for ($i = 0; $i < $iter; $i++) { $t = $t * $mul % $mod; }
+echo $t."\n";
