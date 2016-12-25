b06fd501fdb424da99cb9d1ffab4a1f7f96baef5
Henrik Svenning Pedersen <henrik.s.pedersen@gmail.com>
2016-12-25 11:06:50 +0000

    2016-24, animation

--- 2016/24/24.php
+++ 2016/24/24.php
@@ -11,6 +11,8 @@ $mustVisit = [];
 $distances = [0 => []];
 $visitedStates = [];
 
+$animate = true;
+
 $mePos = false;
 foreach ($input as $y => $row) {
 	foreach (str_split($row) as $x => $val) {
@@ -21,45 +23,54 @@ foreach ($input as $y => $row) {
 				$mustVisit[$val] = [$y, $x];
 			}
 		}
-		$grid[$y][$x] = ($val !== "#");
+		$grid[$y][$x] = is_numeric($val) ? $val : ($val !== "#");
 	}
 }
 $queue[0] = [new World($grid, $mePos)];
+
 foreach ($mustVisit as $v => $pos) {
 	$queue[$v] = [new World($grid, $pos)];
 	$distances[$v] = [];
 	$visitedStates[$v] = [];
 }
 
+$c = 0;
+$prevDepth = 0;
 foreach ($queue as $initPos => &$worlds) {
-	echo "starting at $initPos\n";
+	if ($animate) {
+		$animateGrid = new World($grid, $mePos);
+	}
 	while ($world = array_shift($worlds)) {
-		#echo "-------------------------\n";
-		#echo $world."\n";
 		$hash = md5((string)$world);
 		if (isset($visitedStates[$initPos][$hash])) {
-			#echo "already been here " . implode(",", $world->pos()) . "\n";
 			continue;
 		}
-		#echo "first time at " . implode(",", $world->pos()) . "\n";
+		if ($animate && $world->steps() != $prevDepth) {
+			system("clear");
+			echo $animateGrid->colorFormat()."\n";
+			echo "starting at $initPos, depth: {$world->steps()}";
+			#usleep(20000);
+		}
+		$prevDepth = $world->steps();
 		$visitedStates[$initPos][$hash] = true;
 		$to = $world->isAtAnyOf($mustVisit);
 		if ($to !== false && $to != $initPos) {
-			#echo "hopp: $to: {$world->steps()}\n";
 			$distances[$initPos][$to] = $world->steps();
 		}
 		foreach ($world->getNextLevel() as $w2) {
 			$worlds[] = $w2;
+			if ($animate) {
+				$animateGrid->moveMe($w2->pos()[0], $w2->pos()[1]);
+			}
 		};
-		#echo "world count (at $initPos): " . count($worlds)."\n";
+		$c++;
 	}
 }
 
 $initDistances = $distances[0];
 $mustVisitDistances = array_diff_key($distances, [0 => true]);
-var_dump($initDistances, $mustVisitDistances);
 $routes = allRoutes($initDistances, $mustVisitDistances);
-var_dump($routes);
+
 echo "\nPart 1: " . min($routes) . "\n";
 
 $p2dist = [];
@@ -67,14 +78,13 @@ foreach ($routes as $route => $length) {
 	$from = $route[strlen($route) - 1];
 	$p2dist[$route."_0"] = $length + $distances[0][$from];
 }
-var_dump($p2dist);
+
 echo "\nPart 2: " . min($p2dist) . "\n";
 
 
 
 function allRoutes($initDistances, $mustVisitDistances, $length = 0, $route = "")
 {
-	echo "here we go ($route = $length)!\n";
 	$routes = [];
 	foreach ($initDistances as $pos => $dist) {
 		$newMvd = $mustVisitDistances;
@@ -89,7 +99,6 @@ function allRoutes($initDistances, $mustVisitDistances, $length = 0, $route = ""
 			unset($mvd[$pos]);
 		}
 		if (count($newMvd) == 0) {
-			echo "found a match! (" . $route . $pos . "_)" . ($length + $dist) . "\n";
 			return [$route . "_" . $pos => $length + $dist];
 		} else {
 			$routes = array_merge($routes, allRoutes($newInitDistance, $newMvd, $length + $dist, $route . $pos . "_"));
@@ -98,13 +107,13 @@ function allRoutes($initDistances, $mustVisitDistances, $length = 0, $route = ""
 	return $routes;
 }
 
-#var_dump($distances);
-
 class World
 {
 	private $_mePos = false;
 	private $_grid = false;
 	private $_steps = 0;
+
+	private $_visited = [];
 
 	public function __construct($world, $mePos)
 	{
@@ -148,27 +157,48 @@ class World
 				if ($this->_grid[$y+$ty][$x+$tx] === false) {
 					continue;
 				}
+				if (isset($this->_visited[$y+$ty][$x+$tx])) {
+					continue;
+				}
 
+				//suboptimized for better animation
 				$newGrid = clone $this;
-				#try {
-					$newGrid->moveMe($y + $ty, $x + $tx);
-				#} catch (Exception $e) {
-				#	continue;
-				#}
+				$newGrid->moveMe($y + $ty, $x + $tx);
 				$nextLevel[] = $newGrid;
 			}
 		}
 		return $nextLevel;
 	}
 
-	private function moveMe($y, $x)
+	public function moveMe($y, $x)
 	{
+		$this->_visited[$this->_mePos[0]][$this->_mePos[1]] = true;
 		$this->_mePos = [$y, $x];
 	}
 
 	public function __clone()
 	{
 		$this->_steps++;
+	}
+
+	public function colorFormat()
+	{
+		$ret = "";
+		foreach ($this->_grid as $y => $row) {
+			foreach ($row as $x => $val) {
+				$colored = (isset($this->_visited[$y]) && isset($this->_visited[$y][$x]));
+				$ret .= $colored ? "\e[42m" : "";
+				if ($y == $this->_mePos[0] && $x == $this->_mePos[1]) {
+					$ret .= "@";
+				} else {
+					$ret .= is_numeric($val) ? "\e[41m$val\e[0m" : (($val) ? " " : "#");
+				}
+				$ret .= $colored ? "\e[0m" : "";
+			}
+			$ret .= "\n";
+		}
+
+		return $ret;
 	}
 
 	public function __toString()
@@ -179,12 +209,11 @@ class World
 				if ($y == $this->_mePos[0] && $x == $this->_mePos[1]) {
 					$ret .= "@";
 				} else {
-					$ret .= ($val) ? " " : "#";
+					$ret .= is_numeric($val) ? $val : (($val) ? " " : "#");
 				}
 			}
 			$ret .= "\n";
 		}
-		$ret .= "@" . implode(",", $this->_mePos) . "\n";
 
 		return $ret;
 	}
