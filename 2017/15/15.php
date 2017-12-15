a367c3efcc2b7427ecd12150b45a968a0cf49436
Henrik Svenning Pedersen <henrik.s.pedersen@gmail.com>
2017-12-15 08:39:09 +0000

    2017-15, optimal runtime in php?

--- 2017/15/15.php
+++ 2017/15/15.php
@@ -4,45 +4,30 @@ ini_set('memory_limit','2048M');
 
 $test = isset($argv[1]) && $argv[1] == "test";
 
-$aVal = ($test) ? 65 : 783;
-$bVal = ($test) ? 8921 : 325;
+$a = ($test) ? 65 : 783;
+$b = ($test) ? 8921 : 325;
 
 $sum = 0;
 
 for ($i = 0; $i < 40000000; $i++) {
-    $aVal = ($aVal * 16807) % 2147483647;
-    $bVal = ($bVal * 48271) % 2147483647;
-    if (($aVal & 65535) == ($bVal & 65535)) {
+    $a = ($a * 16807) % 2147483647;
+    $b = ($b * 48271) % 2147483647;
+    if (($a & 0xFFFF) == ($b & 0xFFFF)) {
         $sum++;
     }
 }
-echo "Part 1: " . $sum . "\n";
+echo "Part 1: $sum\n";
 
+$a = ($test) ? 65 : 783;
+$b = ($test) ? 8921 : 325;
+$sum = 0;
 
-$aVal = ($test) ? 65 : 783;
-$bVal = ($test) ? 8921 : 325;
-
-$sum = $i = 0;
-
-while (true) {
-    $aVal = ($aVal * 16807) % 2147483647;
-    #echo $aVal . ": " . ($aVal & 3)."\n";
-    if (($aVal & 3) == 0) {
-        while (true) {
-            $bVal = ($bVal * 48271) % 2147483647;
-            if (($bVal & 7) == 0) {
-                $i++;
-                if (($aVal & 65535) == ($bVal & 65535)) {
-                    $sum++;
-                    #echo "$sum@$i\n";
-                }
-
-                if ($i == 5000000) {
-                    break 2;
-                }
-                break;
-            }
-        }
+for ($i = 0; $i < 5000000; $i++) {
+    do $a = ($a * 16807) % 2147483647; while ($a & 0x3);
+    do $b = ($b * 48271) % 2147483647; while ($b & 0x7);
+    if (($a & 0xFFFF) == ($b & 0xFFFF)) {
+        $sum++;
     }
 }
-echo "Part 2: " . $sum . "\n";
+
+echo "Part 2: $sum\n";
