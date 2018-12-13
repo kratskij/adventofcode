d7502f2d0aca10b80d1f62a745c7e42e87084e13
Henrik <h.pedersen@sportradar.com>
2018-12-13 12:13:31 +0000

    2018-13, part2 working, but still crappy code. will fix?

--- 2018/13/13.php
+++ 2018/13/13.php
@@ -17,14 +17,9 @@ define("CART_UP", 1);
 define("CART_RIGHT", 2);
 define("CART_DOWN", 3);
 
-define("TRACK_UPDOWN", "|/\+");
-define("TRACK_LEFTRIGHT", "-/\+");
-
 define ("ISECT_LEFT", 0);
 define ("ISECT_STRAIGHT", 1);
 define ("ISECT_RIGHT", 2);
-
-
 
 foreach ($input as $k => $line) {
     $grid[$k] = str_split($line);
@@ -41,17 +36,10 @@ foreach ($grid as $y => $row) {
     foreach ($row as $x => $val) {
         if (isset($dirMap[$val])) {
             $carts[] = ["x" => $x, "y" => $y, "in" => true, "dir" => $dirMap[$val], "nextIsect" => ISECT_LEFT];
-
-            if (
-                strpos(TRACK_UPDOWN, $grid[$y-1][$x]) !== false &&
-                strpos(TRACK_UPDOWN, $grid[$y+1][$x]) !== false
-            ) {
-                $grid[$y][$x] = "|";
-            } else if (
-                strpos(TRACK_LEFTRIGHT, $grid[$y][$x-1]) !== false &&
-                strpos(TRACK_LEFTRIGHT, $grid[$y][$x+1]) !== false
-            ) {
+            if ($val == "<" || $val == ">") {
                 $grid[$y][$x] = "-";
+            } elseif ($val == "^" || $val == "v") {
+                $grid[$y][$x] = "|";
             } else {
                 throw new Exception("NO0");
             }
@@ -64,8 +52,15 @@ $seconds = 0;
 $dieAtNext = false;
 while (true) {
     echo "NEW SECOND!" . ++$seconds."\n";
+    usort($carts, function($a, $b) {
+        if ($a["y"] == $b["y"]) {
+            return $a["x"] - $b["x"];
+        }
+        return $a["y"] - $b["y"];
+    });
     for ($idx = 0; $idx < count($carts); $idx++) {
         #echo "IDX " . $idx."\n";
+
         if ($carts[$idx]["in"]) {
             switch ($carts[$idx]["dir"]) {
                 case CART_LEFT:
@@ -76,20 +71,10 @@ while (true) {
                     break;
                 case CART_UP:
                     $atNextPos = $grid[--$carts[$idx]["y"]][$carts[$idx]["x"]];
-                    if (!isset($grid[$carts[$idx]["y"]][$carts[$idx]["x"]])) {
-                        var_Dump($carts[$idx], $seconds, $idx);
-                        die();
-                    }
                     break;
                 case CART_DOWN:
                     $atNextPos = $grid[++$carts[$idx]["y"]][$carts[$idx]["x"]];
                     break;
-            }
-            if ($idx == 3) {
-                #echo $c["y"].",".$c["x"].",",$grid[$c["y"]][$c["x"]]."\n";
-            }
-            if (!isset($grid[$carts[$idx]["y"]][$carts[$idx]["x"]])) {
-                #var_dump("OUT OF GRID", $c, $idx); die();
             }
             switch ($atNextPos) {
                 case "/":
@@ -103,7 +88,7 @@ while (true) {
                         $carts[$idx]["dir"] = CART_LEFT;
                     }
                     break;
-                case '\\':
+                case "\\":
                     if ($carts[$idx]["dir"] == CART_LEFT) {
                         $carts[$idx]["dir"] = CART_UP;
                     } else if($carts[$idx]["dir"] == CART_RIGHT) {
@@ -132,43 +117,55 @@ while (true) {
                     $carts[$idx2]["in"] = false;
 
                     echo "CRASH AT " . $carts[$idx]["x"] . "," . $carts[$idx]["y"] . " after $seconds seconds!\n";
+                    #die();
                     #break 3;
                 }
             }
 
 
-            if ($dieAtNext !== false) {
-                var_Dump(array_filter($carts, function($c) { return $c["in"]; }));
-                die();
-            }
-            echo "LEFT: " . count(array_filter(array_column($carts, "in")))."\n";
-            if (count(array_filter(array_column($carts, "in"))) == 1) {
-                #var_Dump(array_filter($carts, function($c) { return $c["in"]; }));
-                #die();
-                $dieAtNext = true;
-            }
+
         } else {
             #echo "outcart?";
         }
-    }
-
-    for ($idx3 = 0; $idx3 < count($carts); $idx3++) {
-        echo $idx3;
-        if (!$carts[$idx3]["in"]) {
-            continue;
-        }
-        for ($idx2 = 0; $idx2 < count($carts); $idx2++) {
-            if ($idx2 != $idx3 && $carts[$idx2]["in"] && $carts[$idx2]["x"] == $carts[$idx3]["x"] && $carts[$idx2]["y"] == $carts[$idx3]["y"]) {
-                $carts[$idx3]["in"] = false;
-                $carts[$idx2]["in"] = false;
-
-                echo "IT HAPPENED AT " . $carts[$idx3]["x"] . "," . $carts[$idx3]["y"] . " after $seconds seconds!\n";
-                die();
-                #break 3;
+        if ($dieAtNext !== false) {
+            $out = "";
+            foreach ($grid as $y => $row) {
+                foreach ($row as $x => $val) {
+                    $cartFound = false;
+                    $cartCopy = $carts;
+                    foreach ($cartCopy as $idx3 => $cart) {
+                        if ($cart["x"] == $x && $cart["y"] == $y) {
+                            if (!$cart["in"]) {
+                                $out .= "\033[1;31mX\033[0m";
+                            } else {
+                                $out .= "\033[1;37m".array_flip($dirMap)[$cart["dir"]]."\033[0m";
+                            }
+                            $cartFound = true;
+                            break;
+                        }
+                    }
+                    if (!$cartFound) {
+                        $out .= $val;
+                    }
+                }
+                $out .= "\n";
             }
+            $out .= "\n";
+            echo $out;
+            var_Dump(array_filter($carts, function($c) { return $c["in"]; }));
+            if (++$dieAtNext > 0) {
+                die();
+            }
+            continue 2;
+        }
+        echo "LEFT: " . count(array_filter(array_column($carts, "in")))."\n";
+        if (count(array_filter(array_column($carts, "in"))) == 1) {
+            #var_Dump(array_filter($carts, function($c) { return $c["in"]; }));
+            #die();
+            $dieAtNext = 0;
         }
     }
-    /*
+/*
     $out = "";
     foreach ($grid as $y => $row) {
         foreach ($row as $x => $val) {
@@ -177,9 +174,9 @@ while (true) {
             foreach ($cartCopy as $idx3 => $cart) {
                 if ($cart["x"] == $x && $cart["y"] == $y) {
                     if (!$cart["in"]) {
-                        $out .= "X";
+                        $out .= "\033[1;31mX\033[0m";
                     } else {
-                        $out .= array_flip($dirMap)[$cart["dir"]];
+                        $out .= "\033[1;37m".array_flip($dirMap)[$cart["dir"]]."\033[0m";
                     }
                     $cartFound = true;
                     break;
@@ -192,10 +189,10 @@ while (true) {
         $out .= "\n";
     }
     $out .= "\n";
-    system(clear);
+    #system("clear");
     echo $out;
-    #usleep(500);
-    */
+    usleep(1000000);
+*/
 
 #var_Dump($grid);
 }
@@ -205,3 +202,5 @@ echo $val;
 
 #p2: 95,54
 #p2: 96,54
+#p2: 94,54
+#p2: 93,54
