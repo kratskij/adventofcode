8dd348acd2b016a3649da01488927cb3ac21fd94
Henrik Svenning Pedersen <henrik.s.pedersen@gmail.com>
2018-12-13 21:12:42 +0000

    2018-13: a bit of cleanup, plus better animation

--- 2018/13/13.php
+++ 2018/13/13.php
@@ -22,21 +22,24 @@ define ("ISECT_LEFT", 0);
 define ("ISECT_STRAIGHT", 1);
 define ("ISECT_RIGHT", 2);
 
-foreach ($input as $k => $line) {
-    $grid[$k] = str_split($line);
+foreach ($input as $line) {
+    $grid[] = str_split($line);
 }
 
-$dirMap = [
-    "<" => CART_LEFT,
-    ">" => CART_RIGHT,
-    "^" => CART_UP,
-    "v" => CART_DOWN,
-];
+$dirMap = array_flip(["<", "^", ">", "v"]);
+
 $carts = [];
 foreach ($grid as $y => $row) {
     foreach ($row as $x => $val) {
         if (isset($dirMap[$val])) {
-            $carts[] = ["x" => $x, "y" => $y, "in" => true, "dir" => $dirMap[$val], "nextIsect" => ISECT_LEFT];
+            $carts[] = [
+                "x" => $x,
+                "y" => $y,
+                "alive" => true,
+                "deadSince" => false,
+                "dir" => $dirMap[$val],
+                "isectDir" => ISECT_LEFT
+            ];
             if ($val == "<" || $val == ">") {
                 $grid[$y][$x] = "-";
             } elseif ($val == "^" || $val == "v") {
@@ -49,7 +52,6 @@ foreach ($grid as $y => $row) {
     }
 }
 
-$seconds = 0;
 $remainingCarts = $carts;
 while (true) {
     usort($carts, function($a, $b) {
@@ -58,110 +60,133 @@ while (true) {
         }
         return $a["y"] - $b["y"];
     });
-    for ($idx = 0; $idx < count($carts); $idx++) {
-        if ($carts[$idx]["in"]) {
-            switch ($carts[$idx]["dir"]) {
-                case CART_LEFT:
-                    $atNextPos = $grid[$carts[$idx]["y"]][--$carts[$idx]["x"]];
-                    break;
-                case CART_RIGHT:
-                    $atNextPos = $grid[$carts[$idx]["y"]][++$carts[$idx]["x"]];
-                    break;
-                case CART_UP:
-                    $atNextPos = $grid[--$carts[$idx]["y"]][$carts[$idx]["x"]];
-                    break;
-                case CART_DOWN:
-                    $atNextPos = $grid[++$carts[$idx]["y"]][$carts[$idx]["x"]];
-                    break;
-            }
-            switch ($atNextPos) {
-                case "/":
-                    if ($carts[$idx]["dir"] == CART_LEFT) {
-                        $carts[$idx]["dir"] = CART_DOWN;
-                    } else if ($carts[$idx]["dir"] == CART_RIGHT) {
-                        $carts[$idx]["dir"] = CART_UP;
-                    } else if ($carts[$idx]["dir"] == CART_UP) {
-                        $carts[$idx]["dir"] = CART_RIGHT;
-                    } else if ($carts[$idx]["dir"] == CART_DOWN) {
-                        $carts[$idx]["dir"] = CART_LEFT;
+    foreach ($carts as $idx => &$c) {
+        if (!$c["alive"]) {
+            continue;
+        }
+        switch ($c["dir"]) {
+            case CART_LEFT:
+                $nextPosValue = $grid[$c["y"]][--$c["x"]];
+                break;
+            case CART_RIGHT:
+                $nextPosValue = $grid[$c["y"]][++$c["x"]];
+                break;
+            case CART_UP:
+                $nextPosValue = $grid[--$c["y"]][$c["x"]];
+                break;
+            case CART_DOWN:
+                $nextPosValue = $grid[++$c["y"]][$c["x"]];
+                break;
+        }
+        switch ($nextPosValue) {
+            case "/":
+                if ($c["dir"] == CART_LEFT) {
+                    $c["dir"] = CART_DOWN;
+                } else if ($c["dir"] == CART_RIGHT) {
+                    $c["dir"] = CART_UP;
+                } else if ($c["dir"] == CART_UP) {
+                    $c["dir"] = CART_RIGHT;
+                } else if ($c["dir"] == CART_DOWN) {
+                    $c["dir"] = CART_LEFT;
+                }
+                break;
+            case "\\":
+                if ($c["dir"] == CART_LEFT) {
+                    $c["dir"] = CART_UP;
+                } else if($c["dir"] == CART_RIGHT) {
+                    $c["dir"] = CART_DOWN;
+                } else if ($c["dir"] == CART_UP) {
+                    $c["dir"] = CART_LEFT;
+                } else if ($c["dir"] == CART_DOWN) {
+                    $c["dir"] = CART_RIGHT;
+                }
+                break;
+            case "+":
+                if ($c["isectDir"] == ISECT_LEFT) {
+                    $c["dir"]--;
+                    if ($c["dir"] == -1) {
+                        $c["dir"] = CART_DOWN;
                     }
-                    break;
-                case "\\":
-                    if ($carts[$idx]["dir"] == CART_LEFT) {
-                        $carts[$idx]["dir"] = CART_UP;
-                    } else if($carts[$idx]["dir"] == CART_RIGHT) {
-                        $carts[$idx]["dir"] = CART_DOWN;
-                    } else if ($carts[$idx]["dir"] == CART_UP) {
-                        $carts[$idx]["dir"] = CART_LEFT;
-                    } else if ($carts[$idx]["dir"] == CART_DOWN) {
-                        $carts[$idx]["dir"] = CART_RIGHT;
-                    }
-                    break;
-                case "+":
-                    if ($carts[$idx]["nextIsect"] == ISECT_LEFT) {
-                        $carts[$idx]["dir"]--;
-                        if ($carts[$idx]["dir"] == -1) {
-                            $carts[$idx]["dir"] = CART_DOWN;
-                        }
-                    } else if ($carts[$idx]["nextIsect"] == ISECT_RIGHT) {
-                        $carts[$idx]["dir"] = ($carts[$idx]["dir"] + 1) % 4;
-                    }
-                    $carts[$idx]["nextIsect"] = ($carts[$idx]["nextIsect"] + 1) % 3;
-            }
+                } else if ($c["isectDir"] == ISECT_RIGHT) {
+                    $c["dir"] = ($c["dir"] + 1) % 4;
+                }
+                $c["isectDir"] = ($c["isectDir"] + 1) % 3;
+        }
 
-            for ($idx2 = 0; $idx2 < count($carts); $idx2++) {
-                if ($idx2 != $idx && $carts[$idx2]["in"] && $carts[$idx2]["x"] == $carts[$idx]["x"] && $carts[$idx2]["y"] == $carts[$idx]["y"]) {
-                    $carts[$idx]["in"] = false;
-                    $carts[$idx2]["in"] = false;
+        for ($idx2 = 0; $idx2 < count($carts); $idx2++) {
+            if ($idx2 != $idx && $carts[$idx2]["alive"] && $carts[$idx2]["x"] == $c["x"] && $carts[$idx2]["y"] == $c["y"]) {
+                $c["alive"] = false;
+                $carts[$idx2]["alive"] = false;
+                $carts[$idx2]["deadSince"] = 0;
 
-                    static $p1;
-                    if ($p1 === null) {
-                        $p1 = true;
-                        echo "Part 1: " . $carts[$idx]["x"] . "," . $carts[$idx]["y"] . "\n";
-                    }
+                static $p1;
+                if ($p1 === null) {
+                    $p1 = true;
+                    echo "Part 1: " . $c["x"] . "," . $c["y"] . "\n";
                 }
             }
         }
+    }
 
-        if (count($remainingCarts) == 1) {
-            $cart = reset($remainingCarts);
-            echo "Part 2: {$cart['x']},{$cart['y']}\n";
-            die();
-        }
-        $remainingCarts = array_filter(
-            $carts,
-            function($cart) { return $cart["in"]; }
-        );
+    $alive = array_filter($carts, function($cart) { return $cart["alive"]; });
+    if (count($alive) == 1) {
+        $cart = array_shift($alive);
+        echo "Part 2: {$cart['x']},{$cart['y']}\n";
+        die();
     }
 
     if ($animate) {
-        $out = "";
-        foreach ($grid as $y => $row) {
-            foreach ($row as $x => $val) {
-                $cartFound = false;
-                $cartCopy = $carts;
-                foreach ($cartCopy as $idx3 => $cart) {
-                    if ($cart["x"] == $x && $cart["y"] == $y) {
-                        if (!$cart["in"]) {
-                            $out .= "\033[1;31mX\033[0m";
-                        } else {
-                            $out .= "\033[1;37m".array_flip($dirMap)[$cart["dir"]]."\033[0m";
-                        }
-                        $cartFound = true;
-                        break;
-                    }
-                }
-                if (!$cartFound) {
-                    $out .= $val;
-                }
+        foreach ($carts as $idx => $cart) {
+            if (!$carts[$idx]["alive"] && $carts[$idx]["deadSince"] < 50) {
+                $carts[$idx]["deadSince"]++;
             }
-            $out .= "\n";
         }
-        $out .= "\n";
-        system("clear");
-        echo $out;
-        usleep(1000000);
+        animate($grid, $carts, 1000);
     }
 
 }
-echo $val;
+
+function animate($grid, $carts, $interval) {
+    static $frameNum;
+    if ($frameNum === null) {
+        $frameNum = 0;
+    }
+    $frameNum++;
+
+    $out = "";
+    $dirMap = ["◀", "▲", "▶", "▼"];
+    $colorize = [];
+    foreach ($carts as $c) {
+        if ($c["deadSince"] < 50) {
+            for ($i = 0; $i < $c["deadSince"]; $i++) {
+                $colorize[$c["y"]+$i][$c["x"]+($c["deadSince"]-$i)] = true;
+                $colorize[$c["y"]+$i][$c["x"]-($c["deadSince"]-$i)] = true;
+                $colorize[$c["y"]-$i][$c["x"]+($c["deadSince"]-$i)] = true;
+                $colorize[$c["y"]-$i][$c["x"]-($c["deadSince"]-$i)] = true;
+            }
+        }
+    }
+    foreach ($grid as $y => $row) {
+        foreach ($row as $x => $val) {
+            $cartFound = false;
+            foreach ($carts as $idx3 => $cart) {
+                if ($cart["x"] == $x && $cart["y"] == $y) {
+                    $out .= ($cart["alive"] ? "\033[1;32m" : "\033[1;31m") . $dirMap[$cart["dir"]] . "\033[0m";
+                    $cartFound = true;
+                    continue 2;
+                }
+            }
+            if (isset($colorize[$y][$x])) {
+                $out .= "\033[1;31m" . $val . "\033[0m";
+            } else {
+                $out .= $val;
+            }
+        }
+        $out .= "\n";
+    }
+
+    $out .= "\n$frameNum\n\n";
+    system("clear");
+    echo $out;
+    usleep($interval);
+}
