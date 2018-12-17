11e479a0dff656c9a64fcda96ba388bef685b4cd
Henrik Svenning Pedersen <henrik.s.pedersen@gmail.com>
2018-12-17 22:27:42 +0000

    2018-17, cleanup

--- 2018/17/17.php
+++ 2018/17/17.php
@@ -2,138 +2,140 @@
 
 ini_set('memory_limit','2048M');
 
-$test = isset($argv[1]) && $argv[1] == "test";
-$file = ($test) ? "test" : "input";
+define("WALL_HIT",         1);
+define("OUT_OF_RESERVOIR", 2);
+
+define("FLOW",  "💧");
+define("FLOW_TOP", "◠");
+define("STILL", "▒");
+define("CLAY",  "▓");
+define("SAND",  " ");
+
+
+define("LEFT", -1);
+define("RIGHT", 1);
 
 require_once(__DIR__."/../inputReader.php");
 
+$test = isset($argv[1]) && $argv[1] == "test";
+$file = ($test) ? "test" : "input";
+
+
+
 $ir = (new InputReader(__DIR__ . DIRECTORY_SEPARATOR . $file))->trim(true);
-$input = $ir->regex("^(\w)\=([\d\-]+)\,\s(\w)\=([\d\-]+)\.\.([\d\-]+)$");
+$input = $ir->regex("^(\w)\=([\d\-]+)\,\s\w\=([\d\-]+)\.\.([\d\-]+)$");
 
 $grid = [];
 foreach ($input as $k => $line) {
-    list($y, $val1, $x, $from, $to) = $line;
+    list($first, $val1, $from, $to) = $line;
     for ($i = $from; $i <= $to; $i++) {
-        if ($y == "x") {
-            $grid[$i][$val1] = "#";
-        } else if ($y == "y") {
-            $grid[$val1][$i] = "#";
-        }
+        $grid[($first == "y") ? $val1 : $i][ ($first == "y") ? $i : $val1] = CLAY;
     }
 }
 
 $minY = min(array_keys($grid));
 $maxY = max(array_keys($grid));
 
-$fillers = [
-    ["y" => $minY, "x" => 500]
-];
 
-$filled = [];
-while ($fillers) {
-    $newFillers = [];
-    foreach ($fillers as $key => &$filler) {
-        $x = &$filler["x"];
-        $y = &$filler["y"];
 
-        //fall until we reach something
-        while (!isset($grid[$y][$x])) {
-            $grid[$y][$x] = "|";
-            $filled[$y."_".$x] = true;
-            $y++;
-            if ($y > $maxY) { // We reched the bottom
-                continue 2;
-            }
-        }
-
-        if ($grid[$y][$x] == "|") {
-            // we landed in water! gtfo
-            continue;
-        }
-
-        //fill to the sides
-        $xSpread = 0;
-        $rightStopReason = $leftStopReason = false;
-        while (true) {
-            if ($leftStopReason == "WALL" && $rightStopReason == "WALL") {
-                //wall on both sides; step up, start at same position
-                $y--;
-                $xSpread = 0;
-                $leftStopReason = $rightStopReason = false;
-            } else if (
-                ($leftStopReason == "WALL" && $rightStopReason == "FREEFALL") ||
-                ($leftStopReason == "FREEFALL" && $rightStopReason == "WALL") ||
-                ($leftStopReason == "FREEFALL" && $rightStopReason == "FREEFALL")
-            ) {
-                // We're done in this bucket. Fill the top layer with pipes, for some reason
-                $origX = $x;
-                while (isset($grid[$y][$x]) && $grid[$y][$x] == "~") {
-                    $grid[$y][$x] = "|";
-                    $x--;
-                }
-                $x = $origX + 1;
-                while (isset($grid[$y][$x]) && $grid[$y][$x] == "~") {
-                    $grid[$y][$x] = "|";
-                    $x++;
-                }
-                break;
-            }
-
-            if (!$leftStopReason) { // We haven't stopped travelling to the left yet
-                $leftX = $x-$xSpread;
-                if (isset($grid[$y][$leftX]) && $grid[$y][$leftX] == "#") { // we hit a wall
-                    $leftStopReason = "WALL";
-                } else if (@$grid[$y+1][$leftX] == "|") { //we hit a stream! Annoying bug
-                    #printGrid($grid, $fillers, $filled, 5000);
-                    #var_dump($y, $x);
-                    #die('hoi');
-                    $leftStopReason = "FREEFALL";
-                } else {
-                    $grid[$y][$leftX] = "~";
-                    $filled[$y."_".($leftX)] = true;
-                    if (!isset($grid[$y+1][$leftX])) { // we hit an edge! create new filler?
-                        $newFillers[] = ["x" => $leftX, "y" => $y+1];
-                        $leftStopReason = "FREEFALL";
-                    }
-                }
-            }
-
-            if (!$rightStopReason) {  // We haven't stopped travelling to the right yet
-                $rightX = $x+$xSpread;
-                if (isset($grid[$y][$rightX]) && $grid[$y][$rightX] == "#") { // we hit a wall
-                    $rightStopReason = "WALL";
-                } else if (@$grid[$y+1][$rightX] == "|") { //we hit a stream! Annoying bug
-                    #printGrid($grid, $fillers, $filled, 5000);
-                    #var_dump($y, $x);
-                    #die('hoi2');
-                    $leftStopReason = "FREEFALL";
-                } else {
-                    $grid[$y][$rightX] = "~";
-                    $filled[$y."_".($rightX)] = true;
-                    if (!isset($grid[$y+1][$rightX])) { // we hit an edge! create new filler?
-                        $newFillers[] = ["x" => $rightX, "y" => $y+1];
-                        $rightStopReason = "FREEFALL";
-                    }
-                }
-            }
-            $xSpread++;
-        }
+$streamQueue = [ [$minY,500] ];
+while (list($y, $x) = array_shift($streamQueue)) {
+    if (!fall($grid, $y, $x, $maxY)) { // we reached the bottom!
+        continue;
     }
 
-    // replace all the old fillers with the new ones
-    $nf = [];
-    foreach ($newFillers as $f) {
-        $nf[$f["y"]."_".$f["x"]] = $f;
+    if ($grid[$y][$x] == FLOW_TOP) { // we landed in water! gtfo
+        continue;
     }
-    $fillers = $nf;
+
+    foreach (fillReservoir($grid, $y, $x) as $stream) {
+        $streamQueue[] = $stream;
+    }
 }
-#printGrid($grid, $fillers, $filled, 1000000, $minY, $maxY);
 
-echo "Part 1: ". count($filled) . "\n";
-echo "Part 2: ". array_sum(array_map(function($row) { return count(array_filter($row, function($r) { return $r == "~"; })); }, $grid)) . "\n";
 
-function printGrid($grid, $fillers, $filled, $sleep = 0, $from, $to) {
-    #system("clear");
+
+$still = $flow = 0;
+foreach ($grid as $row) {
+    foreach ($row as $g) {
+        if ($g == STILL) {
+            $still++;
+        } else if ($g == FLOW || $g == FLOW_TOP) {
+            $flow++;
+        }
+    }
+}
+
+printGrid($grid, $minY, $maxY);
+echo "Part 1: " . ($still + $flow) . "\n";
+echo "Part 2: $still\n";
+
+
+
+function fall(&$grid, &$y, $x, $stop) {
+    while (!isset($grid[$y][$x])) {
+        $grid[$y][$x] = FLOW;
+        if (++$y > $stop) { // We reached the bottom
+            return false;
+        }
+    }
+    return true;
+}
+
+function fillSideways(&$grid, $y, $x, $direction, &$streams) {
+    while (true) {
+        if (isset($grid[$y][$x]) && $grid[$y][$x] == CLAY) { // we hit a wall
+            return WALL_HIT;
+        } else if (@$grid[$y+1][$x] == FLOW) { //we hit a stream! Annoying bug
+            return OUT_OF_RESERVOIR;
+        } else {
+            $grid[$y][$x] = STILL;
+            if (!isset($grid[$y+1][$x])) { // we hit an edge! create new filler?
+                $streams[] = [$y+1, $x, ];
+                return OUT_OF_RESERVOIR;
+            }
+        }
+        $x += $direction;
+    }
+}
+
+function fillReservoir(&$grid, $y, $x) {
+    //fill to both sides until we're out of the reservoir. return new stream(s)
+    $streams = [];
+    while (true) {
+        $left = fillSideWays($grid, $y, $x, LEFT, $streams);
+        $right = fillSideWays($grid, $y, $x, RIGHT, $streams);
+
+        if ($left == WALL_HIT && $right == WALL_HIT) {
+            //wall on both sides; step up, start at same position
+            $y--;
+            $left = $right = false;
+        } else if (
+            ($left == WALL_HIT && $right == OUT_OF_RESERVOIR) ||
+            ($left == OUT_OF_RESERVOIR && $right == WALL_HIT) ||
+            ($left == OUT_OF_RESERVOIR && $right == OUT_OF_RESERVOIR)
+        ) {
+            // We're done in this bucket. Fill the top layer with pipes, for some reason
+            $origX = $x;
+            while (isset($grid[$y][$x]) && $grid[$y][$x] == STILL) {
+                $grid[$y][$x] = FLOW_TOP;
+                $x--;
+            }
+            $x = $origX + 1;
+            while (isset($grid[$y][$x]) && $grid[$y][$x] == STILL) {
+                $grid[$y][$x] = FLOW_TOP;
+                $x++;
+            }
+            break;
+        }
+
+    }
+
+    return $streams;
+}
+
+function printGrid($grid, $from, $to, $sleep = 0) {
+    // system("clear");
     $maxX = -INF;
     $minX = INF;
     foreach ($grid as $g) {
@@ -144,16 +146,12 @@ function printGrid($grid, $fillers, $filled, $sleep = 0, $from, $to) {
     for ($y = $from; $y <= $to; $y++) {
         echo str_pad($y, 5, " ", STR_PAD_RIGHT);
         for ($x = $minX; $x <= $maxX; $x++) {
-            if (isset($filled[$y."_".$x])) {
-                echo "\033[1;32m";
+            if (isset($grid[$y][$x])) {
+                #echo "\033[1;32m";
             }
-            if (isset($fillers[$y . "_" . $x])) {
-                echo "▼";
-            } else {
-                echo isset($grid[$y][$x]) ? $grid[$y][$x] : ".";
-            }
+            echo isset($grid[$y][$x]) ? $grid[$y][$x] : SAND;
             if (isset($filled[$y."_".$x])) {
-                echo "\033[0m";
+                #echo "\033[0m";
             }
         }
         echo "\n";
