14d8479cb5323462508ccbe80f2283c1af7d14d7
Henrik <h.pedersen@sportradar.com>
2019-12-16 15:17:03 +0000

    2019-16

--- 2019/16/16.php
+++ 2019/16/16.php
@@ -9,104 +9,66 @@ require_once(__DIR__."/../inputReader.php");
 
 $ir = new InputReader(__DIR__ . DIRECTORY_SEPARATOR . $file);
 $input = $ir->trim()->raw();
-$basePattern = [0, 1, 0, -1];
 
-echo "Part 1: " . convert2($input, $basePattern, 100, 0) . "\n";
+echo "Part 1: " . convert($input, 100) . "\n";
 
 $newInput = "";
 #$inputAsStr = implode("", $input);
+
 for ($i = 0; $i < 10000; $i++) {
     $newInput .= $input;
 }
-echo "Part 2: " . convert2($newInput, $basePattern, 100, substr($newInput, 0, 7)) . "\n";
+echo "Part 2: " . convert($newInput, 100, substr($newInput, 0, 7)) . "\n";
 
 
-function convert2($input, $basePattern, $phases, $offset) {
-    echo "Offset: " . $offset . "\n";
-    echo "Input length: " . strlen($input)."\n";
-    $adds = $subs = [];
-    $inputCount = strlen($input);
-    for ($c = 0; $c < $inputCount; $c++) {
-        $tmp = 0;
-        for ($k = 0; $k < $inputCount; $k++) {
-            $bPos = (floor((($k+1) % (4 * ($c+1))) / ($c + 1))) % 4;
-            if ($bPos == 1) {
-                $adds[$c][] = $k;
-            } else if ($bPos == 2) {
-                $subs[$c][] = $k;
-            }
-
-        }
-    }
-    echo "DONE with prejob\n";
-    for ($p = 1; $p <= $phases; $p++) {
-        $sum = 0;
-        foreach ($adds as $a) {
-            $sum += substr($a, -1);
-        }
-        foreach ($subs as $s) {
-            $sum -= substr($s, -1);
-        }
-        $newInput = array_sum($adds) - array_sum($subs);
-        for ($c = 0; $c < $inputCount; $c++) {
-            $input[$c] = $newInput
-        }
-        $newInput = "";
-        /*$inputCount = strlen($input);
-        for ($c = 0; $c < $inputCount; $c++) {
-            $tmp = 0;
-            for ($k = 0; $k < $inputCount; $k++) {
-                $bPos = (floor((($k+1) % (4 * ($c+1))) / ($c + 1))) % 4;
-                $tmp += (int)$input[$k] * $basePattern[$bPos];
-                if (($k % 1000) == 0) {
-                    echo $k."\n";
-                }
-            }
-
-            $newInput .= substr($tmp, -1);
-
-        }*/
-        $input = $newInput;
-        echo "$p: " . substr($input, 0, 8) . "\n";
-    }
-
-    return substr($input, $offset, 8);
+/*for ($i = 0; $i < 4; $i++) {
+    $newInput .= $input;
 }
+echo "Part 2: " . convert2($newInput, $basePattern, 100, 0/* substr($newInput, 0, 2)*/
+#) . "\n";
 
 
+function convert($originalInput, $phases, $offset = 0) {
+    $input = substr($originalInput, $offset);
+    $inputCount = strlen($input);
 
-function convert($input, $basePattern, $phases) {
+    if ($inputCount <= strlen($originalInput) / 2) {
+        // Reversed, we can safely accumulate the last half; they will all be ones.
+        for ($p = 1; $p <= $phases; $p++) {
+            $tmp = 0;
+            for ($c = $inputCount - 1; $c >= 0; $c--) {
+                $tmp += $input[$c];
+                $input[$c] = abs($tmp % 10);
+            }
+        }
+
+        return substr($input, 0, 8);
+    }
+
     for ($p = 1; $p <= $phases; $p++) {
-        $newInput = [];
+        $newInput = $input;
+        for ($c = 0; $c <= $inputCount; $c++) {
+            $tmp = 0;
+            $coffsetone = $c + $offset + 1;
+            for ($k = $c; $k < $inputCount; $k++) { // no need to calculate the first part; they will all be zeros.
+                $bPos = (
+                    floor(
+                        (
+                            ($k + $offset + 1) % (4 * ($coffsetone))
+                        ) / $coffsetone
+                    )
+                ) % 4;
 
-        $inputCount = count($input);
-        foreach ($input as $c => $value) {
-            $tmp = [];
-            $newPattern = [];
-            while (count($newPattern) < $inputCount + 1) {
-                for ($j = 0; $j <= $c; $j++) {
-                    $newPattern[] = $basePattern[0];
-                }
-                for ($j = 0; $j <= $c; $j++) {
-                    $newPattern[] = $basePattern[1];
-                }
-                for ($j = 0; $j <= $c; $j++) {
-                    $newPattern[] = $basePattern[2];
-                }
-                for ($j = 0; $j <= $c; $j++) {
-                    $newPattern[] = $basePattern[3];
+                if ($bPos == 1) {
+                    $tmp += (int)$input[$k];
+                } else if ($bPos == 3) {
+                    $tmp -= (int)$input[$k];
                 }
             }
-            array_shift($newPattern);
-            #echo "pat:" . implode(",", $newPattern)."\n";
-            foreach ($input as $k => $val) {
-                $tmp[] = $val * $newPattern[$k];
-            }
-
-            $newInput[] = substr(array_sum($tmp), -1);
-    #        echo implode(",", $newInput)."\n";
+            $newInput[$c] = abs($tmp % 10);
         }
         $input = $newInput;
     }
-    return substr(implode("", $input), 0, 8);
+
+    return substr($input, 0, 8);
 }
