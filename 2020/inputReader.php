51c84ab6c0f047410349c6411c424abb54a64311
kratskij <>
2020-12-08 08:17:45 +0000

    adding input downloader

--- 2020/inputReader.php
+++ 2020/inputReader.php
@@ -2,9 +2,22 @@
 
 class InputReader {
 
+    private static $_cookie = "session=<YOUR_SESSION_ID>";
+    private static $_year = 2020;
+
     private $rawData;
 
     public function __construct($file) {
+        $fileParts = explode(DIRECTORY_SEPARATOR, $file);
+        $fileName = array_pop($fileParts);
+        if ($fileName == "dl") {
+            $line = readline("Download input file? (Press enter to confirm, Ctrl+C to exit)");
+            $day = (int)array_pop($fileParts);
+            if (!self::autoDownload($day, "input", $file)) {
+                echo "Could not download input file. Aborting.\n";
+            }
+            exit;
+        }
         $this->rawData = file_get_contents($file);
     }
     public function trim($areYouSure = false) {
@@ -60,5 +73,40 @@ class InputReader {
             },
             explode("\n", $this->rawData)
         );
+    }
+
+    private static function autoDownload($day, $file, $inspect = true) {
+        $ch = curl_init (
+            sprintf("https://adventofcode.com/%d/day/%d/input", self::$_year, (int)($day))
+        );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cookie: " . self::$_cookie));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $inputData = curl_exec($ch);
+        file_put_contents($file, $inputData);
+        curl_close($ch);
+
+        if (file_exists($file)) {
+            echo "File downloaded successfully\n";
+            if ($inspect) {
+                echo "--- Inspecting first lines ---\n";
+                $lines = explode("\n", file_get_contents($file));
+                $i = 0;
+                while ($i < 10 && isset($lines[$i])) {
+                    $line = $lines[$i];
+                    if (strlen($line) > 100) {
+                        echo substr($line, 0, 100) . " ... [cut due to long line]\n";
+                    } else {
+                        echo "$line\n";
+                    }
+                    $i++;
+                }
+                echo "---\n";
+                echo count($lines) . " lines in total\n";
+                echo "--- End of inspection ---\n";
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
