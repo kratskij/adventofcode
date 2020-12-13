17d0d5877e9cdd65196868c2e94616dcb46764b3
kratskij <>
2020-12-13 13:12:21 +0000

    2020-13-13: using lcm, so it works for any input (not just prime numbers)

--- 2020/13/13.php
+++ 2020/13/13.php
@@ -23,29 +23,54 @@ foreach (explode(",", $lines[1]) as $i) {
 $i = $earliest;
 $increment = 1;
 
+$matches = [];
 while (!$p1 || !$p2) {
-    $all = true;
-    $matched = [];
-    $newMax = 1;
     foreach ($buses as $busId => $busOffset) {
         if (!$p1) {
             if (($i % $busId) == 0) {
                 $p1 = ($i - $earliest) * $busId;
             }
-            $all = false;
         } else {
             if ((($i + $busOffset) % $busId) == 0) {
-                $newMax *= $busId;
-            } else {
-                $all = false;
+                $matches[$busId] = $busId;
             }
         }
     }
-    $increment = max($increment, $newMax);
-    if ($all) {
+    $increment = max($increment, lcm($matches));
+    if (count($matches) == count($buses)) {
         $p2 = $i;
     }
     $i += $increment;
 }
 
 echo "P1: $p1\nP2: $p2\n";
+
+function lcm(array $args) {
+    foreach ($args as $arg) {
+        if ($arg == 0) {
+            return 0;
+        }
+    }
+    if (empty($args)) {
+        return 0;
+    }
+    if (count($args) == 1) {
+        return reset($args);
+    }
+    if (count($args) == 2) {
+        $m = array_shift($args);
+        $n = array_shift($args);
+        return abs(($m * $n) / gcd($m, $n));
+    }
+
+    return lcm(array_merge([array_shift($args)], [lcm($args)]));
+}
+
+function gcd($a, $b) {
+   while ($b != 0) {
+       $t = $b;
+       $b = $a % $b;
+       $a = $t;
+   }
+   return $a;
+}
