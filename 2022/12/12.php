6361a5213d58f6a66205df900a5815497b6d3016
Henrik S. Pedersen <h.pedersen@sportradar.com>
2022-12-12 07:38:19 +0000

    2022-12: animation

--- 2022/12/12.php
+++ 2022/12/12.php
@@ -9,15 +9,17 @@ require_once(__DIR__."/../inputReader.php");
 
 $ir = (new InputReader(__DIR__ . DIRECTORY_SEPARATOR . $file))->trim(true);
 $grid = $ir->grid();
+$animate = false;
 
 $goalX = $goalY = false;
 $queues = [ 1 => [], 2 => [] ];
 $visiteds = [ 1 => [], 2 => [] ];
 
+$charGrid = $grid;
 foreach ($grid as $y => $row) {
     foreach ($row as $x => $val) {
         if ($val === "S") {
-            $queues[1][] = [$y, $x, 0];
+            $queues[1][] = [$y, $x, 0, [$y => [$x => true]]];
             $visiteds[1][$y][$x] = true;
             $grid[$y][$x] = "a";
         } else if ($val === "E") {
@@ -26,7 +28,7 @@ foreach ($grid as $y => $row) {
             $grid[$y][$x] = "z";
         }
         if ($grid[$y][$x] == "a") {
-            $queues[2][] = [$y, $x, 0];
+            $queues[2][] = [$y, $x, 0, [$y => [$x => true]]];
             $visiteds[2][$y][$x] = true;
         }
         $grid[$y][$x] = ord($grid[$y][$x]) - 96;
@@ -39,7 +41,7 @@ $results = [];
 foreach ($queues as $part => $queue) {
     $visited = $visiteds[$part];
     while ($n = array_shift($queue)) {
-        list($currentY, $currentX, $score) = $n;
+        list($currentY, $currentX, $score, $trace) = $n;
 
         if ($currentY == $goalY && $currentX == $goalX) {
             $results[$part] = $score;
@@ -55,8 +57,20 @@ foreach ($queues as $part => $queue) {
                 $grid[$newY][$newX] - $grid[$currentY][$currentX] <= 1 // increase > 1 is too steep
             ) {
                 $visited[$newY][$newX] = true;
-                $queue[] = [$newY, $newX, $score + 1];
+                $newTrace = $trace;
+                $newTrace[$newY][$newX] = true;
+                $queue[] = [$newY, $newX, $score + 1, $newTrace];
+                $color  = 32;
+            } else {
+                $color = 31;
             }
+
+            if ($animate) {
+                system("clear");
+                printGrid($grid, $newY, $newX, $color, $trace, $charGrid);
+                usleep(1000);
+            }
+            #sleep(1);
         }
     }
 }
@@ -64,3 +78,35 @@ $p1 = $results[1];
 $p2 = $results[2];
 
 echo "P1: $p1\nP2: $p2\n";
+
+function printGrid($grid, $curY, $curX, $color, $trace, $charGrid) {
+    $minY = $minX = PHP_INT_MAX;
+    $maxY = $maxX = -PHP_INT_MAX;
+    foreach ($grid as $y => $row) {
+        $minY = min($minY, $y);
+        $maxY = max($maxY, $y);
+        $minX = min($minX, min(array_keys($row)));
+        $maxX = max($maxX, max(array_keys($row)));
+    }
+
+    $out = "";
+    for ($y = $minY; $y <= $maxY; $y++) {
+        for ($x = $minX; $x <= $maxX; $x++) {
+            if ($y == $curY && $x == $curX) {
+                $out .= colorize($charGrid[$y][$x], $color);
+            } else if (isset($grid[$y][$x])) {
+                if (isset($trace[$y][$x])){
+                    $out .= $charGrid[$y][$x];
+                } else {
+                    $out .= " ";
+                }
+            }
+        }
+        $out .= "\n";
+    }
+
+    echo $out;
+}
+function colorize($string, $color) {
+    return sprintf("\033[%sm%s\033[0m", $color, $string);
+}
