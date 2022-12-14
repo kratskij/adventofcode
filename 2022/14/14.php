d606a25d3e28befa3dac83779d77324068ae5069
Henrik S. Pedersen <h.pedersen@sportradar.com>
2022-12-14 07:56:59 +0000

    2022-14, cleanup

--- 2022/14/14.php
+++ 2022/14/14.php
@@ -10,34 +10,22 @@ require_once(__DIR__."/../Util.php");
 
 $ir = (new InputReader(__DIR__ . DIRECTORY_SEPARATOR . $file))->trim(true);
 $input = $ir->csv(" -> ");
-$p1 = $p2 = 0;
-$grid = [];
 
 const WALL = 1;
 const SAND = 2;
 
+$p1 = $p2 = 0;
+$grid = [];
 foreach ($input as $k => $line) {
     $prevY = $prevX = false;
     foreach ($line as $pos) {
         list($x, $y) = array_map("intval", explode(",", $pos));
         $grid[$y][$x] = WALL;
-        if ($prevY) {
-            $dx = 0; $dy = 0;
-            while ($prevY+$dy !== $y || $prevX+$dx !== $x) {
-                if ($y > $prevY) {
-                    $dy++;
-                } else if ($y < $prevY) {
-                    $dy--;
+        if ($prevY !== false) {
+            for ($ny = min($y, $prevY); $ny <= max($y, $prevY); $ny++) {
+                for ($nx = min($x, $prevX); $nx <= max($x, $prevX); $nx++) {
+                    $grid[$ny][$nx] = WALL;
                 }
-                if ($x > $prevX) {
-                    $dx++;
-                } else if ($x < $prevX) {
-                    $dx--;
-                }
-                if (!isset($grid[$prevY+$dy])) {
-                    $grid[$prevY+$dy] = [];
-                }
-                $grid[$prevY+$dy][$prevX+$dx] = WALL;
             }
         }
         $prevY = $y;
@@ -45,57 +33,41 @@ foreach ($input as $k => $line) {
     }
 }
 
-$i = 0;
+$startY = 0;
+$startX = 500;
+
+$settled = 0;
 $maxY = max(array_keys($grid));
-$countp1 = true;
 
 while (true) {
-    $sandX = 500;
-    $sandY = 0;
-    while (
-        $sandY < $maxY + 1 && (
-            !isset($grid[$sandY+1]) ||
-            !isset($grid[$sandY+1][$sandX]) ||
-            !isset($grid[$sandY+1][$sandX-1]) ||
-            !isset($grid[$sandY+1][$sandX+1])
-        )
-    ) {
-        if (!isset($grid[$sandY+1]) || !isset($grid[$sandY+1][$sandX])) {
-            if ($sandY+1 > $maxY) {
-                $countp1 = false;
+    $y = $startY;
+    $x = $startX;
+    while ($y < $maxY + 1) {
+        if (!isset($grid[$y+1]) || !isset($grid[$y+1][$x])) {
+            // nothing below us; do nothing/fall further
+            if (!$p1 && $y+1 > $maxY) {
+                // we're spilling below the lowest point for the first time; p1 is done.
+                $p1 = $settled;
             }
-            $sandY++;
-            continue;
-        }
-
-        if (!isset($grid[$sandY+1][$sandX-1])) {
-            $sandX--;
-            $sandY++;
-            continue;
-        } else if (!isset($grid[$sandY+1][$sandX+1])) {
-            $sandX++;
-            $sandY++;
-            continue;
-        } else if (isset($grid[$sandY+1][$sandX])) {
+        } else if (!isset($grid[$y+1][$x-1])) {
+            $x--;
+        } else if (!isset($grid[$y+1][$x+1])) {
+            $x++;
+        } else if (isset($grid[$y+1][$x])) {
+            $stable = true;
+            // we found stable groud; settle here
             break;
         }
-        $sandY++;
-        if ($sandY > $maxY) {
-            break 2;
-        }
+        $y++;
     }
 
-    $grid[$sandY][$sandX] = SAND;
-    if ($countp1) {
-        $p1++;
-    }
-    $p2++;
+    $grid[$y][$x] = SAND;
+    $settled++;
 
-    if ($sandY == 0 && $sandX == 500) {
+    if ($y == $startY && $x == $startX) {
         break;
     }
 }
+$p2 = $settled;
 
 echo "P1: $p1\nP2: $p2\n";
-
-#657
